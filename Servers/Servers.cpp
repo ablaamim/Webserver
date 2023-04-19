ba336feda766d494b7cdc101a85c20ae614f72af
ablaamim <aku-no-hana@outlook.fr>
2023-04-19 16:09:49 +0000

    more

--- Servers/Servers.cpp
+++ Servers/Servers.cpp
@@ -7,51 +7,71 @@ void     Servers::new_server_create_socket(std::string ip, std::string port)
     int         socket_fd;
 
     socket_info.ip = ip;
-    socket_info.port = std::stod(port);
+    //std::cout << "port: " << port << std::endl;
+    //std::cout << "ip: " << ip << std::endl;
+    socket_info.port = port;
     socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     socket_info.socket_fd = socket_fd;
-    socket_info.option = 1;
-
     if (!socket_fd)
     {
-        throw Server_err("Error creating socket");
+        throw Server_err(" : Error : creating socket");
     }
-    if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &(socket_info.option), sizeof(socket_info.option) < 0))
+    socket_info.option = 1;
+    
+    // SOL_SOCKET is the socket layer itself
+    // SO_REUSEADDR allows the local address to be reused when the server is restarted
+    // SO_REUSEPORT allows the local port to be reused when the server is restarted
+
+    if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &socket_info.option, sizeof(socket_info.option)) < 0)
     {
         close(socket_fd);
         //throw Server_err("Error setting socket options");
     }
 
     socket_info.address.sin_family = AF_INET;
-    socket_info.address.sin_addr.s_addr = inet_addr(socket_info.ip.c_str());
-    socket_info.address.sin_port = htons(socket_info.port);
+    socket_info.address.sin_addr.s_addr = inet_addr(ip.c_str());
+    socket_info.address.sin_port = htons(std::atoi(port.c_str()));
     socket_info.address_len = sizeof(socket_info.address);
 
-    if (bind(socket_fd, (struct sockaddr *)&socket_info.address, socket_info.address_len) < 0)
+    if (bind(socket_fd, (struct sockaddr *) &socket_info.address, socket_info.address_len) < 0)
     {
         close(socket_fd);
-        //throw Server_err("Error binding socket");
+        //throw Server_err(" : Error : binding socket");
     }
         
-    sockIpPort.insert(std::make_pair(socket_fd, socket_info));
+    socket_ip_port.insert(std::make_pair(socket_fd, socket_info));
     
     //std::cout << "Socket created" << std::endl;
 }
 
+void Servers::listen_for_connections()
+{
+    std::cout << "Listening for connections" << std::endl;
+    std::cout << "----------------------------------------" << std::endl;
+    for (socket_map_t::iterator iter = socket_ip_port.begin(); iter != socket_ip_port.end(); iter++)
+    {
+        //std::cout << "Listening for connections on socket " << iter->first << std::endl;
+        if (listen(iter->first, 3) < 0)
+        {
+            close(iter->first);
+            //throw Server_err("Error listening for connections");
+        }
+    }
+}
 
 Servers::Servers(configurationSA &config)
 {
     std::cout << "Servers constructor called" << std::endl;
     std::cout << "----------------------------------------" << std::endl;
     
-    int number_of_sockets = 0;
+    //int number_of_sockets = 0;
 
     configurationSA::data_type conf = config.get_data();
 
     std::set <std::pair<std::string, std::string> > bind_sockets;
     try
     {
-        // Loop through the configuration file and create a socket for each server
+        // Loop through the configuration file and create 1 single socket for each server
         for (configurationSA::data_type::iterator iterConf = conf.begin(); iterConf != conf.end(); iterConf++)
         { 
             //std::cout << "FIRST LOOP " << std::endl;
@@ -60,7 +80,6 @@ Servers::Servers(configurationSA &config)
                 //std::cout << "SECOND LOOP " << std::endl;
                 for (std::set<std::string>::iterator iterSet = iterListen->second.begin(); iterSet != iterListen->second.end(); iterSet++)
                 {
-                
                     if (!bind_sockets.count(std::make_pair(iterListen->first, *iterSet)))
                     {
                         new_server_create_socket(iterListen->first, *iterSet);
@@ -68,19 +87,22 @@ Servers::Servers(configurationSA &config)
                     }
                     //std::cout << "Number of sockets " << sockIpPort.size() << std::endl;
 
-                    std::cout << "\rServer "  << iterConf - conf.begin() << " IP " << iterListen->first << " Port " << *iterSet << std::endl;                
-                    std::cout << "----------------------------------------" << std::endl;
+                    std::cout << "\rServer "  << "0" << "   listening on    " << iterListen->first << "   :  " << *iterSet << "          ...      ";                
                     std::cout.flush();
                     usleep(1000000);
                 }
-            
-                std::cout << "Number of sockets " << ++number_of_sockets << std::endl;
-                std::cout << "----------------------------------------" << std::endl;
+                std::cout << "\rServer "  << iterConf - conf.begin() << "      Up               " << std::endl;
+                //std::cout << "Number of sockets " << ++number_of_sockets << std::endl;
             }
         }
+        listen_for_connections();
     }
     catch (const std::exception& e)
     {
+        for (socket_map_t::iterator iter = socket_ip_port.begin(); iter != socket_ip_port.end(); iter++)
+        {
+            close(iter->first);
+        }
         throw Server_err(e.what());
     }
 }
@@ -88,5 +110,9 @@ Servers::Servers(configurationSA &config)
 Servers::~Servers()
 {
     std::cout << "Servers destructor called" << std::endl;
+    for (socket_map_t::iterator iter = socket_ip_port.begin(); iter != socket_ip_port.end(); iter++)
+    {
+        close(iter->first);
+    }
     std::cout << "----------------------------------------" << std::endl;
 }
--- Servers/Servers.hpp
+++ Servers/Servers.hpp
@@ -17,8 +17,7 @@
 
 class Servers
 {
-    public :
-        
+    public :  
         class socket_t
         {
             public :
@@ -27,7 +26,7 @@ class Servers
                 int         option;
                 int         address_len;
                 std::string ip;
-                int         port;
+                std::string port;
         };
     
     // map of socket_t with the key being the socket file descriptor
@@ -35,15 +34,18 @@ class Servers
 
     private :
 
-        socket_map_t    sockIpPort;
+        socket_map_t    socket_ip_port;
 
         void new_server_create_socket(std::string ip, std::string port);
+        void listen_for_connections();
 
     public :
-
+        // Constructor
         Servers(configurationSA &config);
         ~Servers();
 
+
+    // Exceptions
     class Server_err : public std::exception
     {
         private :
@@ -51,7 +53,7 @@ class Servers
 
         public :
             Server_err(std::string error) : error(error) {}
-            virtual const char* what() const throw() { return error.c_str(); }
+            virtual const char *what() const throw() { return error.c_str(); }
             ~Server_err() throw() {};
     };
 };
