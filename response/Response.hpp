6f4a9e9cf747a7cbea883188b83da0e4e49cc917
Achraf Faris <afaris@e2r13p1.1337.ma>
2023-05-18 17:22:36 +0100

    intialisitaion phaase

--- response/Response.hpp
+++ response/Response.hpp
@@ -40,36 +40,41 @@ class Response : public configurationSA
         once the request.uri (e.g. "/directory/index.php") is matched with the desired location from the config
         the fullPath should be filled along with resourceType.
     */
-    // Request      requestObj;
+
+    // begin of request attributes:
+
+    std::string     method;
+    std::string     host_ip;
+    std::string     host_port;
+    std::string     resoucePath; // like /directory/index.html
+
+    // end of request attributes
+
 
     bool            isCompleted; // whether the reponse has served 100% of resourceLength
-
-    int             resouceLength; // e.g. a video file 10mb 
-    int             length;
-    int             resourceType;
-    std::string     method;
-    std::string     fullPath;
+    int             resouceLength; // e.g. requested resouce size in Bytes
+    int             currentLength; // usefull for chuncking reponses
+    int             resourceType; // MACRO, like DIRECTORY, FILE, CGI_SCRIPT
+    
+    std::string     resourceFullPath;
     std::string     httpVersion;
     std::string     body;
 
-
-    // set this depending on matched location 
-    std::vector<std::string>    allowedMethods;
-
-    // list of status codes and their associated message it should be defined globaly in the main (for later ...)
-    std::map<int, std::string>  statusList;
-
-    // response status code + message (e.g. 200, OK), you can get it from reponseStatus object below
+    // response status code + message (e.g. 200, OK)
     std::pair   <std::string, std::string>  status;
 
     // response headers
     std::map    <std::string, std::string>  headers;
 
-    // Extra arguments depending on resource type (e.g. in case of DIRECTORY, kwargs should contain autoindex as key and its value as value) 
+    /*
+        kwargs or keywords arguments.
+        could contain dynamic args depending on the location.
+        for example it could have ["allowed_methods":"GET, POST, DELETE"]
+        OR/AND "cgi-bin":".py, /bin/python"
+    */
+    std::map    <std::string, std::vector<std::string> >  kwargs;
     
-    std::map    <std::string, std::string>  kwargs;
-    
-    // handle client request
+    // handle client request 
     void    generate();
 
     // convert data members to a response message and write it to the socket
