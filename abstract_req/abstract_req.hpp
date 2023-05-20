c20f2c69eafd4040901631a2a49e5fa04c03de5a
ablaamim <aku-no-hana@outlook.fr>
2023-05-20 11:34:18 +0100

    all data accessible and displayed

--- abstract_req/abstract_req.hpp
+++ abstract_req/abstract_req.hpp
@@ -6,17 +6,30 @@
 class abstract_req
 {
     public :
+        
         abstract_req(int fd = -1)
         {
             _fd = fd;
             //std::cout << "Webserv default constructor" << std::endl;
         };
+        
         ~abstract_req()
         {
             //std::cout << "Webserv default destructor" << std::endl;
         }
 
-        int _fd;
+        int         _fd;
+        
+        //std::string target;
+        
+        // std::string method = "GET";
+        // std::string version = "HTTP/1.1";
+        // std::string host_ip = "127.0.0.1";
+        // std::string host_port = "8080";
+        // std::string path = "/";
+        // size_t max_header_size = 8000;
+        // size_t max_body_size = 1000000;
+
 };
 
 #endif
