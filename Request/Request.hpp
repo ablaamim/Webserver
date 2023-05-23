b1489185d75f1ea7a45345ced20f02b832daf5da
zinb <97052963+zinb-il@users.noreply.github.com>
2023-05-23 19:07:35 +0100

    webserv

--- Request/Request.hpp
+++ Request/Request.hpp
@@ -5,22 +5,25 @@
 #include <map>
 #include <cstring>
 
+typedef std::map<std::string , std::string> param;
+
 class Request
 {
     public:
         std::map<std::string , std::string> params;
-        bool                                headers;
+        bool                                headers_done;
         bool                                first_line;
-        int                                 content_lem;
+        bool                                is_chuncked;
+        int                                 fd_accept;
 
-        int     get_headers(std::string str);
+        int     parse_request(char * str);
+        int     get_headers(char * str);
         void    get_firstline(std::string line);
         void    get_other_lines(std::string line);
         int     check_readed_bytes(void);
         void    print_params();
         Request();
         ~Request();
-
 };
 
 #endif
