615e92e4b84dd49edd8cd2e117a396298aeac566
Achraf Faris <afaris@e2r10p12.1337.ma>
2023-05-24 19:43:22 +0100

    trying to serve chuncked body

--- response/Response.cpp
+++ response/Response.cpp
@@ -1,4 +1,45 @@
 #include "Response.hpp"
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sstream>
+#include <iomanip>
+
+std::string intToHexString(int number) {
+    std::stringstream stream;
+    stream << std::hex << number;
+    return stream.str();
+}
+
+int getFileSize(std::string path)
+{
+    std::ifstream f;
+    f.open(path.c_str(), std::ios::in | std::ios::binary);
+    f.seekg(0, std::ios::end);
+    return f.tellg();
+}
+
+Response::Response(abstract_req req, int id, configurationSA::location location, std::string _client_ip, char **env) : _req(req), clientSocket(id) ,_location(location), _client_ip(_client_ip) , _env(env)
+{
+    // init method map
+    if (!this->_methods.empty())
+        this->_methods.clear();
+    this->_methods.insert(std::pair<std::string, void(Response::*)()>("GET", &Response::handleGet));
+    this->_methods.insert(std::pair<std::string, void(Response::*)()>("POST", &Response::handlePost));
+    this->_methods.insert(std::pair<std::string, void(Response::*)()>("DELETE", &Response::handleDelete));
+
+    this->resourceFullPath = "/Users/afaris/Desktop/Webserver/response/example.html";
+    this->resouceLength = getFileSize(this->resourceFullPath);
+    this->currentLength = 0;
+    this->lastChunkSize = 0;
+    this->isCompleted = false;
+    this->httpVersion = "HTTP/1.1";
+    this->headers.insert(std::pair<std::string, std::string>("Server", "webserv"));
+    this->status = std::pair<std::string, std::string>("200", "OK");
+    this->fd = open(this->resourceFullPath.c_str(), O_RDONLY);
+    std::cout << "fd: " << this->fd << std::endl;
+};
 
 bool isDirectory(std::string path)
 {
@@ -12,18 +53,12 @@ bool isDirectory(std::string path)
         return false;
 }
 
-// this function returns the type of the resource
-
 int getResourceType(std::string path, std::map<std::string, std::vector<std::string> > kwargs)
 {
-    // check if kwargs has cgi-bin or not
-    // if yes, return CGI
     if (kwargs.find("cgi-bin") != kwargs.end())
         return CGI;
     else if (kwargs.find("return") != kwargs.end())
         return REDIRECT;
-    // check if the path is a directory
-    // if yes, return DIRECTORY
     if (isDirectory(path))
         return DIRECTORY;
     return FILE;
@@ -37,65 +72,64 @@ void    Response::init()
     this->resouceLength = 0;
 }
 
-// Response::Response(int id, configurationSA::location location, char **env)
-// {
-//     this->httpVersion = "HTTP/1.1";
-//     this->clientSocket = id;
-//     this->location = location;
-//     this->resourceFullPath = "/Users/afaris/abs/response/";
-//     this->isCompleted = false;
-//     this->headers["Server"] = "webserv";
-//     this->headers["Content-Type"] = "text/html";
-//     this->init();
-// }
-
-Response::Response(int id)
-{
-    //this->clientSocket = id;
-    this->isCompleted = false;
-    this->currentLength = 0;
-    this->resouceLength = 0;
-    this->resourceType = NONE;
-    this->resourceFullPath = "/Users/afaris/abs/response/";
-    this->httpVersion = "HTTP/1.1";
-    this->status = std::make_pair("200", "OK");
-    this->headers["Server"] = "webserv";
-    this->headers["Content-Type"] = "text/html";
-    this->init();
-}
-
 Response::~Response()
 {
      //std::cout << "Response destructor" << std::endl;
 }
 
+void    Response::generateBody()
+{
+    char buf[BUFFER_SIZE] = {0};
+    this->lastChunkSize = read(this->fd, buf, BUFFER_SIZE - 1);
+    std::cout << "last chunk size: " << this->lastChunkSize << std::endl;
+    buf[this->lastChunkSize] = '\0';
+    if (this->lastChunkSize == -1)
+        throw std::runtime_error("Error reading file");
+    this->currentLength += this->lastChunkSize;
+    this->body.append(buf);
+}
 
 void    Response::serve()
 {
     std::string responseMessage;
-
-    responseMessage = this->httpVersion + " " + this->status.first + " " + this->status.second + "\r\n";
-    for (std::map<std::string, std::string>::iterator it = this->headers.begin(); it != this->headers.end(); it++)
+    this->headers["transfer-encoding"] = "chunked";
+    this->headers["content-type"] = "text/html";
+    responseMessage += this->httpVersion + " " + this->status.first + " " + this->status.second + "\r\n";
+    std::map<std::string, std::string>::iterator it = this->headers.begin();
+    while (it != this->headers.end())
+    {
         responseMessage += it->first + ": " + it->second + "\r\n";
+        it++;
+    }
+    generateBody();
     responseMessage += "\r\n";
-
-    responseMessage += this->body;
+    responseMessage += intToHexString(this->lastChunkSize) + "\r\n";
+    responseMessage.append(this->body) + "\r\n";
+    responseMessage += "0\r\n\r\n";
     send(this->clientSocket, responseMessage.c_str(), responseMessage.length(), 0);
-    this->isCompleted = true;
+    if (this->currentLength >= this->resouceLength)
+        this->isCompleted = true;
 }
-
 
 Response::Response(const Response &other)
 {
+    std::cout << "Response copy constructor" << std::endl;
+    // copy all the attributes of the other object to this object
     this->clientSocket = other.clientSocket;
-    this->isCompleted = other.isCompleted;
+    this->resourceFullPath = other.resourceFullPath;
     this->resouceLength = other.resouceLength;
     this->currentLength = other.currentLength;
-    this->resourceType = other.resourceType;
-    this->resourceFullPath = other.resourceFullPath;
+    this->lastChunkSize = other.lastChunkSize;
+    this->isCompleted = other.isCompleted;
     this->httpVersion = other.httpVersion;
-    this->body = other.body;
-    this->status = other.status;
     this->headers = other.headers;
-    this->kwargs = other.kwargs;
+    this->status = other.status;
+    this->fd = other.fd;
+    this->resourceType = other.resourceType;
+    this->body = other.body;
+    this->_req = other._req;
+    this->_location = other._location;
+    this->_client_ip = other._client_ip;
+    this->_env = other._env;
+
 }
