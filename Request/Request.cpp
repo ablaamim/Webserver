bd4b1c50d9081e15cd0a3acd8639f4b36121f157
zinb <97052963+zinb-il@users.noreply.github.com>
2023-05-30 15:10:43 +0100

    webserv

--- Request/Request.cpp
+++ Request/Request.cpp
@@ -9,12 +9,23 @@ void Request::print_body()
     std::cout << COLOR_BLUE << "Body size : " << COLOR_RESET << this->body.size() << std::endl;
 }
 
+std::vector<char>   Request::get_body()
+{
+    return this->body;
+}
+std::string Request::get_content()
+{
+    return this->params[_CONTENT_];
+}
+
 void    Request::print_params()
 {
     it_param it = this->params.begin();
+    std::cout << COLOR_RED << "   Display Request params " << COLOR_RESET << std::endl;
     while (it != this->params.end())
     {
-        std::cout << COLOR_BLUE << it->first << " : " << COLOR_RESET << it->second << std::endl;
+        if (it->first != _CONTENT_)
+            std::cout << COLOR_BLUE << it->first << " : " << COLOR_RESET << it->second << std::endl;
         it++;
     }
 }
@@ -25,7 +36,7 @@ size_t Request::get_body_size()
     return (this->params[_CONTENT_].length());
 }
 
-Request::Request(int fd) : _fd(fd)
+Request::Request()
 {
     this->headers_done = false;
     this->first_line = false;
@@ -77,7 +88,7 @@ void Request::get_firstline(std::string line)
     std::stringstream   file(line);
     int                 i = 0;
 
-    // std::cout << "Parsing First line " << std::endl;
+    //std::cout << "Parsing First line " << std::endl;
     while (std::getline(file, str, ' '))
     {
         switch (i)
@@ -105,69 +116,59 @@ void Request::get_firstline(std::string line)
 
 void Request::get_other_lines(std::string line)
 {
-    std::string         str1, str2;
-    std::stringstream   file(line);
+    size_t  indx;
 
-    //std::cout << "Parsing Other lines '" << line << "'" << std::endl;
-    if (std::getline(file, str1, ':'))
+    //std::cout << COLOR_GREEN << "Parsing Other lines '" << line << "'" << COLOR_RESET << std::endl;
+    if (this->params.find("Content-Type") != this->params.end())
     {
-        if (std::getline(file, str2, ':'))
-            this->params[str1] = str2.substr(1);
-        else
-        {
-            //std::cout << COLOR_YELLOW << "Parsing Other here  '" << str1 << "'" << COLOR_RESET <<std::endl;
-            this->params[_CONTENT_].append(str1);
-            this->body.insert(this->body.end(), str1.begin(), str1.end());
-            //std::cout << params[_CONTENT_] << std::endl;
-            //std::cout << "Body size : " << this->body.size() << std::endl;
-            //sleep(10);
-        }
+        this->params[_CONTENT_].append(line);
+        return ;
     }
+    indx = line.find(": ");
+    if (indx != std::string::npos )
+        this->params[line.substr(0, indx)] = line.substr(indx + 2);
 }
 
-int Request::get_headers(char * str)
+int Request::get_headers(std::string str)
 {
-    char *line;
+    size_t line;
 
-    // std::cout << "Parsing headers " << std::endl;
-    line = std::strtok(str, "\r\n");
-    if (!line)
-        return _ERR_PARSE_REQUEST;
-    while(line)
+    if (!str.size())
+        return 1;
+    //std::cout << "Parsing headers " << std::endl;
+    while((line = str.find("\r\n")) != std::string::npos)
     {
         if (!this->first_line)
-            this->get_firstline(std::string(line));
+            this->get_firstline(str.substr(0, line));
         else
-            this->get_other_lines(std::string(line));
-        line = std::strtok(NULL, "\r\n");
+            this->get_other_lines(str.substr(0, line));
+        str = str.substr(line + 2);
     }
+    if (str.size())
+        this->get_other_lines(str);
     this->headers_done = true;
     return (check_readed_bytes());
 }
 
-int Request::get_chuncked_msg(char * str)
+int Request::get_chuncked_msg(std::string str)
 {
-    char *line;
+    size_t line;
 
-    std::cout << "Parsing chuncked messages " << std::endl;
     // std::cout << COLOR_RED << "----------------------------------" << std::endl;
-    // std::cout << COLOR_GREEN << str << std::endl << std::endl;
+    // std::cout << COLOR_GREEN << this->params[_CONTENT_] << std::endl << std::endl;
     // std::cout << COLOR_RED << "----------------------------------" << std::endl;
-    line = std::strtok(str, "0\r\n\r\n");
-    if (!line)
-        return _ERR_PARSE_REQUEST;
-    this->params[_CONTENT_].append(std::string(line));
-    
-    this->body.insert(this->body.end(), line, line + std::strlen(line));
-
-    //std::cout << this->body.size() << std::endl;
-    //sleep(10);
-
+    while((line = str.find("0\r\n\r\n")) != std::string::npos)
+    {
+        this->params[_CONTENT_].append(str.substr(0, line));
+       // this->body.insert(this->body.end(), line, line + std::strlen(line));
+        str = str.substr(line + 5);
+    }
+    this->params[_CONTENT_].append(str);
     // std::cout << COLOR_RED << "----------------------------------" << std::endl;
     // std::cout << COLOR_BLUE << this->params[_CONTENT_] << std::endl << std::endl;
     // std::cout << COLOR_YELLOW << std::strlen(line) << "   " << std::strlen(str) << std::endl << std::endl;
     // std::cout << COLOR_RED << "----------------------------------" << std::endl;
-    // return (check_readed_bytes());
+    //return (check_readed_bytes());
     return (0);
 }
 
@@ -176,7 +177,7 @@ int Request::parse_request(char * str)
     if (!str)
         return _ERR_PARSE_REQUEST;
     else if (!this->headers_done)
-        return (this->get_headers(str));
+        return (this->get_headers(std::string(str)));
     else if (this->is_chuncked)
         return(this->get_chuncked_msg(str));
     return 1;
