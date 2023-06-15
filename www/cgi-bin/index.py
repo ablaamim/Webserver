591cd945557bc546ad6b55480b2f0a6edd92d3c9
zinb <97052963+zinb-il@users.noreply.github.com>
2023-06-15 14:10:24 +0100

    test cgi

--- www/cgi-bin/index.py
+++ www/cgi-bin/index.py
@@ -10,12 +10,12 @@ form = cgi.FieldStorage()
 first_name = form.getvalue('first_name')
 last_name  = form.getvalue('last_name')
 
-print "Content-type:text/html\r\n\r\n"
-print "<html>"
-print "<head>"
-print "<title>Hello - Second CGI Program</title>"
-print "</head>"
-print "<body>"
-print "<h2>Hello %s %s</h2>" % (first_name, last_name)
-print "</body>"
-print "</html>"
+print ("Content-type:text/html\r\n\r\n")
+print ("<html>")
+print ("<head>")
+print ("<title>Hello - Second CGI Program</title>")
+print ("</head>")
+print ("<body>")
+print ("<h2>Hello %s %s</h2>"%(first_name, last_name))
+print ("</body>")
+print ("</html>")
