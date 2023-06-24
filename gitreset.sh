655da2be1fdf447842efd3f7fb4f146d89e7722a
Achraf Faris <afaris@student.1337.ma>
2023-06-24 17:07:32 +0100

    ok

--- gitreset.sh
+++ gitreset.sh
@@ -1,16 +1,11 @@
-git filter-branch --env-filter '
-WRONG_EMAIL="afaris@e2r3p7.1337.ma"
-NEW_NAME="Achraf Faris"
-NEW_EMAIL="afaris@student.1337.ma"
-
-if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
-then
-    export GIT_COMMITTER_NAME="$NEW_NAME"
-    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
-then
-    export GIT_AUTHOR_NAME="$NEW_NAME"
-    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
-fi
-' --tag-name-filter cat -- --branches --tags
+git filter-branch --commit-filter '
+        if [ "$GIT_COMMITTER_NAME" = "Achraf Faris" ];
+        then
+                GIT_COMMITTER_NAME="Achraf Faris";
+                GIT_AUTHOR_NAME="Achraf Faris";
+                GIT_COMMITTER_EMAIL="afaris@student.1337.ma";
+                GIT_AUTHOR_EMAIL="afaris@student.1337.ma";
+                git commit-tree "$@";
+        else
+                git commit-tree "$@";
+        fi' HEAD
